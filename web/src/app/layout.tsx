578c1b3a6604a9924c5f923410c3ebdce3afd425
Radek Sienkiewicz <mail@velvetshark.com>
2025-08-22 16:01:03 +0200

    Add og:image

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,8 +5,31 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://gitping.vlvt.sh'),
   title: 'GitPing - GitHub Release Notifications',
   description: 'Get instant notifications when your favorite repositories release new versions.',
+  openGraph: {
+    title: 'GitPing - GitHub Release Notifications',
+    description: 'Get instant notifications when your favorite repositories release new versions.',
+    url: 'https://gitping.vlvt.sh',
+    siteName: 'GitPing',
+    images: [
+      {
+        url: '/gitping.png',
+        width: 1200,
+        height: 630,
+        alt: 'GitPing - GitHub Release Notifications',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'GitPing - GitHub Release Notifications',
+    description: 'Get instant notifications when your favorite repositories release new versions.',
+    images: ['/gitping.png'],
+  },
   icons: {
     icon: '/favicon.png',
     shortcut: '/favicon.png',
